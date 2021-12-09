36fdb91564cb8cf3aa7bd2124545a80f42fa0984
Michael Stramel <mstramel@coolfiresolutions.com>
2021-12-09 16:14:31 -0600

    add open-props lib

--- website/src/components/game-hub/GameHub.ts
+++ website/src/components/game-hub/GameHub.ts
@@ -6,7 +6,7 @@ const logo = new URL('/assets/open-wc-logo.svg', import.meta.url).href;
 @customElement('game-hub')
 export class GameHub extends LitElement {
   @property({ type: String })
-  title = 'My app';
+  title = 'Game Hub';
 
   static styles = css`
     :host {
